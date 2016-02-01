5be2bc85734921d9494bfd8f1165f98af600dcd7
Samantha Doran <samanthadoran3@gmail.com>
2016-02-01 18:25:37 -0500

    Account for some cases in step1 test file

--- nim-samanthadoran/reader.nim
+++ nim-samanthadoran/reader.nim
@@ -45,10 +45,42 @@ proc read_atom(reader: Reader): malData =
   try:
     let val: int = parseInt(peek(reader))
     result = malData(malType: malNumber, kind: malNumber, num: val)
-  except ValueError:
-    result = malData(malType: malSymbol, kind: malSymbol, sym: peek(reader))
-  finally:
     next(reader)
+  except ValueError:
+    let val = peek(reader)
+
+    #Keep track of things...
+    next(reader)
+
+    #Account for special symbols
+    case val
+    of "'":
+      var list: seq[malData] = @[]
+      list.add(malData(malType: malSymbol, kind: malSymbol, sym: "quote"))
+      list.add(read_form(reader))
+      result = malData(malType: malList, kind: malList, list: list)
+
+    of "`":
+      var list: seq[malData] = @[]
+      list.add(malData(malType: malSymbol, kind: malSymbol, sym: "quasiquote"))
+      list.add(read_form(reader))
+      result = malData(malType: malList, kind: malList, list: list)
+
+    of "~":
+      var list: seq[malData] = @[]
+      list.add(malData(malType: malSymbol, kind: malSymbol, sym: "unquote"))
+      list.add(read_form(reader))
+      result = malData(malType: malList, kind: malList, list: list)
+
+    of "~@":
+      var list: seq[malData] = @[]
+      list.add(malData(malType: malSymbol, kind: malSymbol, sym: "splice-unquote"))
+      list.add(read_form(reader))
+      result = malData(malType: malList, kind: malList, list: list)
+
+    else:
+      result = malData(malType: malSymbol, kind: malSymbol, sym: val)
+
 
 proc read_list(reader: Reader): seq[malData] =
   #Allocate space for our list
@@ -65,15 +97,14 @@ proc read_list(reader: Reader): seq[malData] =
 
 proc tokenizer(input: string): seq[string] =
   result = input.findAll(nre.re(malMatch))
+
   #Really gross way to fix whitespace problem
   var toStrip: set[char] = {' ', ','}
   for i in 0..<len(result):
-    result[i] = result[i].strip()
     result[i] = result[i].strip(true, true, toStrip)
 
 proc read_str(input: string): malData =
   let tokens = tokenizer(input)
-  #echo("Number of tokens...", len(tokens))
   var reader: Reader = Reader(position: 0, tokens: tokens)
 
   result = read_form(reader)
